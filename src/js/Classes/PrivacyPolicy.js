690a4404c53410f2cbdfb185e8892b0cd3a347c4
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-08-03 23:19:47 +0200

    Page position after lightbox closed

--- src/js/Classes/PrivacyPolicy.js
+++ src/js/Classes/PrivacyPolicy.js
@@ -8,23 +8,29 @@
 		var el = document.getElementById('privacyPolicy'),
 			body = document.getElementsByTagName('body'),
 			close = $('.js-closeLightbox'),
-			showPrivacyPolicy = $('.js-showPrivacyPolicy');
+			showPrivacyPolicy = $('.js-showPrivacyPolicy'),
+			pagePos;
 		
 		showPrivacyPolicy.on('click', function(e) {
 			e.preventDefault();
-									
+			
+			pagePos = $('body').scrollTop();
 			$(el).toggleClass('is-visible');
 			close.toggleClass('is-visible');
 			$(body).toggleClass('no-scroll');
+			
+			
 				
 		});
 		
 		close.on('click', function(e) {
-			e.preventDefault();
+			e.preventDefault();			
 			
 			$(el).removeClass('is-visible');
 			close.removeClass('is-visible');
 			$(body).removeClass('no-scroll');
+			
+			$(body).scrollTop(pagePos);
 		});
 	};
 		
