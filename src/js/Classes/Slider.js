b4cde639d463c8cb7048a39d1d801fae8f8492ad
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-08-19 22:05:52 +0200

    scroll

--- src/js/Classes/Slider.js
+++ src/js/Classes/Slider.js
@@ -22,9 +22,33 @@
 			});
 		});
 	};
-	
+
 	Slider.prototype.partners = function() {
 
+		var h = ($('.js-logos li').first().width())*6,
+			logos = document.querySelectorAll('.js-logos'),
+			scrollers = document.querySelectorAll('.js-scrollers');
+
+		$(scrollers).width(h*3);
+
+		for (var i = 0; i < 2; i++) {
+			$(logos).clone().insertAfter($(logos));
+		}
+
+		function doScroll() {
+			$('.js-logos:eq(0)').stop().animate(
+				{
+					'margin-left': -h
+				}, 20000, 'linear', function() {
+					$('.js-logos:eq(0)').css('margin-left', 0);
+					doScroll();
+				});
+		};
+
+		doScroll();
+
+
+		/*
 		var el = document.getElementById('partners');
 		
 		$(el).owlCarousel({
@@ -48,6 +72,7 @@
 				}
 			}
 		});
+*/
 	};
 		
 	vtr.Slider = new Slider();
